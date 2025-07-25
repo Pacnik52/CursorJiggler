3d408c41cdb1f788135a5c5011bca96d8763588f
mpachnik <01195943@pe.edu.pl>
2025-07-25 15:55:14 +0200

    ver2

---
+++ jiggler_v2.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import pystray
+import threading
+import time
+import pyautogui
+from PIL import Image
+from pynput import mouse, keyboard
+
+# Po jakim czasie bezczynnosci jest ruszany kursor (w sekundach)
+WAIT_TIME = 25
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+icon_path = resource_path("jigglerIcon.png")
+icon_image = Image.open(icon_path)
+
+def background_task():
+    global last_activity
+    while True:
+        time.sleep(WAIT_TIME)
+        x, y = pyautogui.position()
+        pyautogui.moveTo(x + 1, y)
+        last_activity = time.time()
+
+def after_click(icon, query):
+    if str(query) == "Exit":
+        icon.stop()
+
+def setup_tray():
+    icon = pystray.Icon("Jiggler", icon_image, "CursorJiggler", 
+                        menu=pystray.Menu(
+
+        pystray.MenuItem("Exit", after_click)))
+
+    threading.Thread(target=background_task, daemon=True).start()
+
+    icon.run()
+
+if __name__ == "__main__":
+    setup_tray()
